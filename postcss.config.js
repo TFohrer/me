dd6ccba76ef967b0974afb38e6fe135757c92276
Tarek Fohrer <tarek.fohrer@umwerk.com>
2020-03-16 00:09:13 +0100

    update purge css settings

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
+// removes css that is not used
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project
+    content: [
+        './src/**/*.html'
+    ],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 const plugins =
     process.env.NODE_ENV === 'production'
-        ? ['tailwindcss', 'autoprefixer', '@fullhuman/postcss-purgecss']
+        ? ['tailwindcss', 'autoprefixer', purgecss]
         : ['tailwindcss'];
 
 module.exports = { plugins };
